d6080f63e2b06381eccaa1967f1ea6c20d1d3014
Haley <haleylgoins@gmail.com>
2021-10-13 15:35:19 -0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var questionBank = [
+    {
+        q:"What is HTML ?",
+        choices1:"Haksjaa",
+        choices2:"Hypertext MArkup Language",
+        choices3:"sdfljds",
+        choices4:"sfkshdfk",
+        answer:2
+    },
+    {
+        q:"What is CSS ?",
+        choices1:"Cascading Style Sheets",
+        choices2:"Hypertext MArkup Language",
+        choices3:"sdfljds",
+        choices4:"sfkshdfk",
+        answer:1
+    },
+    {
+        q:"How do you append a string?",
+        choices1:"Cascading Style Sheets",
+        choices2:"Hypertext MArkup Language",
+        choices3:"By using dot notation",
+        choices4:"sfkshdfk",
+        answer:3
+    },
+    {
+        q:"What is the purpose of semicolons?",
+        choices1:"Cascading Style Sheets",
+        choices2:"To show that the line of code is finished",
+        choices3:"By using dot notation",
+        choices4:"sfkshdfk",
+        answer:2
+    },
+]
+var questionContainer = document.getElementById('test-questions');
+var questionContainer = document.getElementById('question-title');
+var questionContainer = document.getElementById('choices-1');
+var questionContainer = document.getElementById('choices-2');
+var questionContainer = document.getElementById('choices-3');
+var questionContainer = document.getElementById('choices-4');
