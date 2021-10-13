5f31b952ac6167b601b93d51445d7f1acef50e66
Haley <haleylgoins@gmail.com>
2021-10-13 15:49:43 -0400

    added questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,8 +33,27 @@ var questionBank = [
     },
 ]
 var questionContainer = document.getElementById('test-questions');
-var questionContainer = document.getElementById('question-title');
-var questionContainer = document.getElementById('choices-1');
-var questionContainer = document.getElementById('choices-2');
-var questionContainer = document.getElementById('choices-3');
-var questionContainer = document.getElementById('choices-4');
+var questionTitle = document.getElementById('question-title');
+var choicesOne = document.getElementById('choices-1');
+var choicesTwo = document.getElementById('choices-2');
+var choicesThree = document.getElementById('choices-3');
+var choicesFour = document.getElementById('choices-4');
+
+questionContainer.style.display = 'none';
+
+var currentQuestion = 0;
+
+var startQuiz = document.querySelector('#start-btn');
+startQuiz.addEventListener('click', function(){
+    questionContainer.style.display = 'block';
+    startQuiz.style.display = 'none';
+    showQuestion();
+})
+
+function showQuestion(){
+questionTitle.textContent = questionBank[currentQuestion].q
+choicesOne.textContent = questionBank[currentQuestion].choices1
+choicesTwo.textContent = questionBank[currentQuestion].choices2
+choicesThree.textContent = questionBank[currentQuestion].choices3
+choicesFour.textContent = questionBank[currentQuestion].choices4
+}
